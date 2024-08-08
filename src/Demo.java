fef99bc4c1633b25d8fbf2bf5857595dd1adb013
Maani Beigy <manibeygi@gmail.com>
2024-08-08 15:44:59 +0200

    :sparkles: adding Demo class for user input

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int Age = sc.nextInt();
+        System.out.println(name + " is " + Age + " years old!");
+    }
+}
